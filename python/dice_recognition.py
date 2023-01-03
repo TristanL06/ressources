6ea20b57f37ca4ea911ffff21098666e9838bef6
TristanL06 <114646770+TristanL06@users.noreply.github.com>
2023-01-03 13:26:33 +0100

    Update dice_recognition.py

--- python/dice_recognition.py
+++ python/dice_recognition.py
@@ -1,3 +1,35 @@
+"""
+guideline :
+Composantes connexes d'un graphe
+
+Défintion:
+Une partie A C G d'un graphe est appelée composante connexe, si pour tout couple de sommets (u,v) de A, il
+existe un chemin de u à v. Un graphe est dit connexe lorsqu'il a une unique composante connexe.
+
+Application:
+Considérons une photo prise à la verticale d'un dé sur une table. On veut déterminer facilement le nombre de
+points sur lą face exposée. Pour cela, on réduit le nombre de couleurs de l'image avec un seuil de niveau de
+gis pour obtenir une image en noir et blanc, de façon que chaque point du dé corresponde à une composante conuexe.
+
+On considère une grille de n x m pixels de valeurs 1 (pour coder la couleur blanche) et 0 (pour coder la couleur
+noire). Cette grille sera implémentée par une liste de listes grille.
+Ainsi grille[i] [j] a pour valeur 1 si le pixel codé est blanc, 0 si le pixel codé est noir.
+
+Algorithme par un parcours en profondeur :
+Pour en profondeur partant d'un sommet u explore tous les sommets atteignables depuis u et seulement
+eux, donc exactement la composante connexe contenant u. On utilise le numéro de la composante courante
+comme marque de visite.
+1. Écrire une fonction dfs_grille récursive prenant en arguments la liste de listes grille, deux entiers i
+et j tels que grille[i][j] est le sommet de départ d'un graphe, marque est le numéro de la composante
+connexe de couleur noire en-cours d'étude et libre est la couleur du pixel à marquer (0 ou 1).
+Cette fonction ne retourne rien mais modife grille[i][j] lorsque grille[i][j] code un pixel noir par
+le numéro de la composante connexe en cours d'étude. Ce numéro est un entier de valeur minimale 2.
+
+sources :
+- https://fr.wikipedia.org/wiki/Graphe_connexe
+- https://fr.wikipedia.org/wiki/Algorithme_de_parcours_en_profondeur
+"""
+
 from PIL import Image
 import requests
 
